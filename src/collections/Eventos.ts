2fbd9f104a480ec432d64ef84d97578ecf674cf0
delavware <delavware@gmail.com>
2024-04-27 14:14:59 -0500

    add events collection

---
+++ src/collections/Eventos.ts
@@ -0,0 +1,68 @@
+import { CollectionConfig } from 'payload/types'
+
+const Eventos: CollectionConfig = {
+  slug: 'eventos',
+  admin: {
+    useAsTitle: 'name',
+  },
+  access: {
+    read: () => true,
+    write: () => true,
+  },
+  fields: [
+    {
+        name: 'nameEvent',
+        label: 'Nombre de evento',
+        type: 'text', 
+        required: true,
+      },
+      {
+        name: 'description',
+        label: 'Sobre el evento',
+        type: 'richText',
+        required: true,
+      },
+      {
+        name: 'dateOnly',
+        type: 'date',
+        admin: {
+          date: {
+            pickerAppearance: 'dayOnly',
+            displayFormat: 'd MMM yyy',
+          },
+        },
+        required: true,
+      },
+      {
+        name: 'timeOnly',
+        type: 'date',
+        admin: {
+          date: {
+            pickerAppearance: 'timeOnly',
+            displayFormat: 'h:mm:ss a',
+          },
+        },
+        required: true,
+      },
+      {
+        name: 'duration',
+        label: 'duracion del evento',
+        type: 'text',
+        required: true,
+      },
+      {
+        name: 'address',
+        label: 'direccion',
+        type: 'text',
+        required: true,
+      },
+      {
+        name: 'speakers',
+        label: 'Presentadores',
+        type: 'text',
+        required: true,
+      },
+  ],
+}
+
+export default Eventos
